71af3738aa194603702c3748a2faad49c4f5fb36
Shrey Gupta <shreygupta97@gmail.com>
2023-05-09 00:27:58 -0400

    Update ESLint and Prettier configs, bump various packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,16 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   extends: ["eslint:recommended", "next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        ignoreRestSiblings: false,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  }
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -5,7 +5,7 @@ module.exports = {
   printWidth: 120,
   tabWidth: 2,
   plugins: ["prettier-plugin-packagejson", "@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
-  importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+  importOrder: ["^([./]|@/)"],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
 };
