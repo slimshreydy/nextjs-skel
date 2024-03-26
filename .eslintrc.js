81413c02d641b0bd1afd4be5e6d8c5acf5362744
Shrey Gupta <shreygupta97@gmail.com>
2024-03-26 01:10:54 -0400

    Add security headers, various extra linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,24 @@
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended", "prettier"],
+  plugins: ["@typescript-eslint", "no-secrets", "unicorn", "promise"],
+  extends: [
+    "eslint:recommended",
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "plugin:unicorn/recommended",
+    "plugin:promise/recommended",
+  ],
   rules: {
+    "no-secrets/no-secrets": "error",
+    "unicorn/no-null": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/prefer-ternary": ["warn", "only-single-line"],
+    "unicorn/filename-case": "off",
+    "unicorn/catch-error-name": "off",
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,56 @@
 /** @type {import('next').NextConfig} */
+
+const CSP_HEADER = {
+  "default-src": "'self'",
+  "script-src": "'self' 'unsafe-inline' va.vercel-scripts.com vercel.live",
+  "style-src": "'self' 'unsafe-inline' vercel.live",
+  "img-src": "'self' blob: data: vercel.com",
+  "font-src": "'self'",
+  "object-src": "'none'",
+  "frame-src": "'self' vercel.live",
+  "base-uri": "'self'",
+  "form-action": "'self'",
+  "frame-ancestors": "'none'",
+  "block-all-mixed-content": "",
+  "upgrade-insecure-requests": "",
+}
+
+if (process.env.NODE_ENV === 'development') {
+  CSP_HEADER["script-src"] += " 'unsafe-eval'"
+}
+
 const nextConfig = {
   poweredByHeader: false,
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          },
+          {
+            key: 'Content-Security-Policy-Report-Only',
+            value: Object.entries(CSP_HEADER).map(([type, policy]) => `${type} ${policy}`).join('; '),
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=(), bluetooth=(), display-capture=(), payment=()'
+          }
+        ],
+      },
+    ]
+  },
 };
 
 export default nextConfig;
